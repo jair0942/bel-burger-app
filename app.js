51690ec8b6ee6db78b8e10e5f3ac14a093f97c94
jair0942 <jairjimenez260@gmail.com>
2026-06-20 12:24:05 -0500

    Agrega cache buster a app.js para forzar nuevo deploy en Vercel

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ function showToast(message, type, duration) {
     toastTimers.push(timer);
 }
 
-// --- Products (Menú real BEL BURGER v2) ---
+// --- Products (Menú real BEL BURGER) ---
 var products = [
     {
         id: 1,
